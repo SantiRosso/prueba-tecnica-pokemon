25b435ceaaadf444e9dfa0217c683282ffcd8c79
SantiRosso <santiagorosso@live.com.ar>
2023-04-25 14:13:16 -0300

    inicializando la app

---
+++ server/src/db.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+require("dotenv").config();
+
+(async () => {
+  mongoose.set("strictQuery", false);
+  const db = await mongoose.connect(`mongodb://${DB_HOST}/${DB_NAME}`);
+  console.log(`Connection stablished to DB: ${db.connection.name}`);
+})();
+
+const connection = mongoose.connection;
+
+connection.once("open", () => {
+  console.log("mongoDB connection stablished");
+});
+
+connection.on("error", (error) => {
+  console.log(error);
+  process.exit(0);
+});
