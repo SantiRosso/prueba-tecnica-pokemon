c8242c656b35fa4b49e24d6ed65f4909bf982f97
SantiRosso <santiagorosso@live.com.ar>
2023-04-26 22:27:15 -0300

    new component: Card

---
+++ client/src/components/card/Card.jsx
@@ -0,0 +1,27 @@
+import { Link } from "react-router-dom"
+//styles
+import s from "./Card.module.css"
+
+const Card = ({name, types, image, id}) => {
+
+    return(
+        <Link className={s.link} to={`/pokemon-detail/${id}`}>
+            <div className={s.card}>
+                
+                <div className={s.divImage}>
+                    <img src={image} alt={name} className={s.image}/>
+                </div>
+                
+                <h4>{name.toUpperCase()}</h4>
+                {
+                    types?.map((e) => {
+                        return(
+                            <span className={s.types}>{e.toUpperCase()}</span>
+                        )
+                    })
+                }
+            </div>
+        </Link>
+    )
+}
+export default Card;
--- client/src/components/views/home/Home.jsx
+++ client/src/components/views/home/Home.jsx
@@ -1,9 +1,11 @@
 import NavBar from "../../navbar/Navbar";
+import Card from "../../card/Card";
 
 const Home = () => {
     return(
        <div>
         <NavBar/>
+        <Card name={"Santi"} types={["fuego", "hielo"]} image={"https://cn.i.cdn.ti-platform.com/content/1149/pokemon-sol-y-luna-ultraleyendas/showpage/ve/showsquare.a6a9b623.png?imwidth=420"}/>
         Home
         </div>
     )
